b8872ce78883f1edf217282e472cb182d5b7c1ea
Имя <e-mail>
2018-02-06 14:23:08 +0300

    newFrontcontroller

---
+++ lessons-php/newFrontcontroller/private/Controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+namespace Vendor\Frontcontroller\Controllers;
+
+class IndexController
+{
+    function indexAction() {
+        echo "IndexController, indexAction";
+    }
+}
+
+?>
---
+++ lessons-php/newFrontcontroller/private/Controllers/RegController.php
@@ -0,0 +1,11 @@
+<?php
+use Vendor\Frontcontroller\Controllers;
+
+class IndexController
+{
+    function indexAction() {
+        echo "IndexController, indexAction";
+    }
+}
+
+?>
